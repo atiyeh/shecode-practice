bae133199e9c9124718707a09c530dee02e92ce7
Atiye.beigirad@gmail.com <atiye.beigirad@gmail.com>
2023-01-26 17:20:56 +0330

    change frame

---
+++ javascript.js
@@ -0,0 +1,5 @@
+apikey = "f1adfdb622o0f53eb34a701c61b4t32f";
+apiurl =
+  "https://api.shecodes.io/weather/v1/current?&key=f1adfdb622o0f53eb34a701c61b4t32f&units=metric";
+city = "Lisbon";
+console.log(apiurl);
