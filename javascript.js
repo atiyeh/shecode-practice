898a0febacf044328fee2d2a568af541f62dc64c
Atiye.beigirad@gmail.com <atiye.beigirad@gmail.com>
2023-01-28 16:12:17 +0330

    fix the style of app

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 apikey = "f1adfdb622o0f53eb34a701c61b4t32f";
-apiurl =
-  "https://api.shecodes.io/weather/v1/current?&key=f1adfdb622o0f53eb34a701c61b4t32f&units=metric";
-city = "Lisbon";
+apiurl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apikey} `;
+
+query = "Lisbon";
 console.log(apiurl);
